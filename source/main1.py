cc5170d1429b0ec17f1ab2cd52231f661ac121ae
ivanvegiani <ivanvegiani@gmail.com>
2018-06-20 20:07:30 -0300

    comitando

--- source/main1.py
+++ source/main1.py
@@ -38,8 +38,8 @@ def update():
         # i=i+1
 def calButtonMouse(event):
     cal1=cal.selection_get()
-
     l11.config(text=str(cal1.day)+"/"+str(cal1.month)+'/'+str(cal1.year))
+    l13.config(text=str(i1.convert_to_doy(cal1)))
 
 #funcionalidades dos botões
 
@@ -51,14 +51,11 @@ def browse_button():
     local_folders.set(filename)
     l9.config(text=local_folders.get())
 
-
-
 i1=autoRmbclib1.RbmcLib()
 
 #
 root=Tk()
 #configurações iniciais
-
 version="versão 2.0"
 now1 = i1.now1
 year=str(now1.year)
@@ -68,7 +65,6 @@ local_folders=StringVar()
 local_folders.set("")
 
 #Threading de update
-
 clock = threading.Timer(30, update)
 clock.start()
 
@@ -86,35 +82,56 @@ else:
 # frames
 frame1=Frame() #superior
 frame2=Frame() # inferior
-
+frame3=Frame()# canvas
+frame5=Frame()
+frame6=Frame()
 # separador
-
 separator = ttk.Separator(orient='horizontal')
-separator.grid(row=10,sticky="we")
+separator.grid(row=1,sticky="we")
+separator1 = ttk.Separator(orient='horizontal')
+separator1.grid(row=1,column=1,sticky="we")
+separator2 = ttk.Separator(orient='horizontal')
+separator2.grid(row=1,column=2,sticky="we")
 
 #frames
-frame1.grid(row=0,column=0)
-frame2.grid(row=11,column=0,sticky="w")
+frame1.grid(row=0,column=0)# ComboBox e botoes
+frame2.grid(row=2,column=0,sticky="nw")#Calendarios e Labels
+frame3.grid(row=2,column=1,sticky="s")#Canvas
+frame5.grid(row=0,column=1)#labels de hoje
+frame6.grid(row=2,column=2,sticky="n")
+#Canvas
+canvas = Canvas(frame3, bg='white', width=200, height=300)
+canvas.grid(row=0,column=0)
+
 
 # Botões
 
 b1=Button(frame1,text="Adicionar")
 b2=Button(frame1,text="Salvar em:",command=browse_button)
 b3=Button(frame1,text="Extrair em:")
-b4=Button(frame2,text="Remover")
-b5=Button(frame2,text="Remover todos")
-b6=Button(frame2,text="Download")
+
+b4=Button(frame6,text="Remover")
+b5=Button(frame6,text="Remover todos")
+b6=Button(frame6,text="Download",font=("Helvetica", 10),fg="red")
 
 #frame1
-b1.grid(row=2,column=4)#Adicionar
-b2.grid(row=4,column=4,pady=15)#Salvar em:
-b3.grid(row=6,column=4,pady=30)#Extrair em:
+b1.grid(row=2,column=4,pady=5)#Adicionar
+b2.grid(row=4,column=4,pady=5)#Salvar em:
+b3.grid(row=6,column=4,pady=5)#Extrair em:
 
-#frame2
-b4.grid(row=0,column=2)
-b5.grid(row=1,column=2)
-b6.grid(row=2,column=10)
+#frame6
+b4.grid(row=0,column=0,sticky="wn")#Remover
+b5.grid(row=3,column=0,sticky="n",pady=20)#Remover todos
+l55=Label(frame6)
+l56=Label(frame6)
+l57=Label(frame6)
+l55.grid(row=2,column=0)#branco
+#l56.grid(row=3,column=0)#branco
+l57.grid(row=4,column=0,pady=20)#branco
+l57.grid(row=4,column=0,pady=50)#branco
+b6.grid(row=5,column=0,pady=10)
 
+b6.grid(row=5,column=0,sticky="S",pady=10)
 # ComboBox
 c1=ttk.Combobox(frame1,width=30)
 c1.grid(row=2,column=2,sticky="w")
@@ -123,34 +140,41 @@ c1.current(2)
 # Calendar
 cal=tkcalendar.Calendar(frame2)
 cal.bind('<<CalendarSelected>>',calButtonMouse)
-
-cal.grid(row=1,column=0,sticky="w")
+cal.grid(row=2,column=0,sticky="w")
 
 # Labels
 l1=Label(frame1,text="AutoRbmcIbge",font=("Helvetica", 20))#fixo
 l2=Label(frame1,text="Bases")#fixo
 l3=Label(frame1,text=version)#fixo
-l4=Label(frame1,text="Data atual:")#fixo
-l5=Label(frame1,text="Dia do ano de hoje:")#fixo
-l6=Label(frame1,text="%s/%s/%s" %(day,month,year))# alterar na execução
-l7=Label(frame1,text=i1.today_gnss)# alterar na execução
-l8=Label(frame1,text="testar") #relógio para testar o update
+l4=Label(frame5,text="Data atual:")#fixo
+l5=Label(frame5,text="Dia do ano de hoje:")#fixo
+l6=Label(frame5,text="%s/%s/%s" %(day,month,year))# alterar na execução
+l7=Label(frame5,text=i1.today_gnss)# alterar na execução
+#l8=Label(frame1,text="testar") #relógio para testar o update
 l9=Label(frame1,text=local_folders.get())
-l10=Label(frame2,text="Dia do levantamento:")
-l11=Label(frame2,text='0')
+#frame 2
+l10=Label(frame2,text="Data do levantamento:")
+l11=Label(frame2,text='0')# data do levantamento
+l12=Label(frame2,text="Dia do ano do levantamento:")
+l13=Label(frame2,text='0')
 
-
-l1.grid(row=0,column=6)
+l1.grid(row=0,column=5)
 l2.grid(row=1,column=2,sticky="w")
 l3.grid(row=0,column=10,sticky="e")
 
-l4.grid(row=2,column=8,columnspan=1,sticky="w") #Data atual
-l6.grid(row=2,column=9)#Data
-l5.grid(row=3,column=8)# dia de ano hoje
-l7.grid(row=3,column=9,sticky="e")# dia de ano
+l4.grid(row=2,column=5,sticky="w") #Data atual
+l6.grid(row=2,column=6)#Data
+l5.grid(row=3,column=5)# dia de ano hoje
+l7.grid(row=3,column=6,sticky="e")# dia de ano
 # l8.grid(row=4,column=9) # testando update
 l9.grid(row=4,column=5,sticky="e")
-l10.grid(row=0,column=0,sticky="w")
-l11.grid(row=0,column=0,sticky="e")
+#frame 2
+l10.grid(row=0,column=0,sticky="w")# data do levantamento
+l11.grid(row=0,column=1,sticky="w")# data do levantamento
+l12.grid(row=1,column=0,sticky="w")# dia do ano do levantamento
+l13.grid(row=1,column=1,sticky="w")#dia do ano do levantamento
+
+
+
 
 root.mainloop()
