163e299123dc3c5a7e478e7e0c96782452ffc117
ivanvegiani <ivanvegiani@gmail.com>
2018-07-27 00:06:20 -0300

    terminado

--- source/autoRmbclib1.py
+++ source/autoRmbclib1.py
@@ -10,6 +10,8 @@ from pathlib import Path
 from socket import gaierror
 import logging
 import threading
+from tkinter import*
+from tkinter import ttk
 
 
 # Configurações iniciais
@@ -76,6 +78,10 @@ def folder_year_function(day_delay):  # define a pasta local relativo ao ano
 
 class RbmcLib():
 
+    fileSize=0
+
+
+
     def __init__(self):
         self.sigla = ('Cascavel - prcv', 'Maringá - prma', 'Curitiba - ufpr', 'Guarapuava - prgu') # siglas das bases
         self.now1=now1
@@ -133,429 +139,35 @@ class RbmcLib():
         ftp.login()
         dir_cwd = ("informacoes_sobre_posicionamento_geodesico/rbmc/dados"+'/'+str(folderYear)+"/"+str(id_target))
 
-        print (dir_cwd)
         print('\nConectado em ftp://geoftp.ibge.gov.br \n')
-        ftp.cwd(str(dir_cwd))
-        p = open(str(os.path.join(paths_local_destino,file_target[i])), "wb")
-        print('Downloading file '+file_target[i]+' para '+str(paths_local_destino))
+
         try:
-            ftp.retrbinary("RETR " + file_target[i], p.write)
+            ftp.cwd(str(dir_cwd))
+        except ftplib.error_perm:
+            pass
+
+        try:
+            p = open(str(os.path.join(paths_local_destino,file_target[i])), "wb")
+            print('Downloading file '+file_target[i]+' para '+str(paths_local_destino))
+        except IndexError:
+            pass
+        # self.fileSize=ftp.size(file_target[i])
+
+        def callback_custom(block):
+            p.write(block)
+
+
+        try:
+            ftp.retrbinary("RETR " + file_target[i], callback_custom)
             p.close()
             print('Download file '+file_target[i]+' sucess\n')
         except ftplib.error_perm:
-
             print('Arquivo '+file_target[i]+' não encontrado no servidor\n')
             p.close()
-            os.remove(os.path.join(paths_local_destino[i],file_target[i]))
+
 
         except TimeoutError:
             print('tempo de conexao expirado')
 
 
         ftp.quit()
-
-
-
-
-    def extracts(paths_extracts,paths_local_destino,file_target,prin=True): # define a descompactação do zip
-        j=0
-        for b in baseFolder:
-            if not os.path.exists(os.path.join(str(paths_local_destino[j]),"extracts")):
-                os.makedirs(os.path.join(str(paths_local_destino[j]),"extracts"))
-            paths_extracts.append(os.path.join(str(paths_local_destino[j]),"extracts"))
-            j=j+1
-        i=0
-        for z in baseFolder:
-
-            try:
-                zip1 = zipfile.ZipFile(str(os.path.join(paths_local_destino[i],file_target[i])))
-                zip1.extractall(str(paths_extracts[i]))
-                if prin:
-                    print('Extraindo para '+str(paths_extracts[i]))
-                logs_info('Extraido '+file_target[i]+' com sucesso')
-                zip1.close()
-            except:
-                os.remove(os.path.join(paths_local_destino[i],file_target[i]))
-                logs_info('Erro '+file_target[i]+' não encontrado')
-            i=i+1
-
-
-    def dia_de_hoje(): # retorna o dia atual em gnss calendar
-        now = datetime.datetime.now()
-        today_gnss=int(date2doy(datetime.date(now.year,now.month,now.day)))
-        return today_gnss
-
-    def conversao_dia(dia,mes,ano): # converte variáveis dia, mes e ano para gnss calendar
-
-        var = datetime.date(ano, mes, dia)
-        alvo=int(date2doy(datetime.date(var.year,var.month,var.day)))
-        logs_bug('alvo', str(alvo))
-        return alvo
-
-
-
-    def reset_folders():
-
-        global folderYear
-        global id_target
-        global file_target
-        global paths_local_destino
-        global path_root
-
-        day = 0
-        folderYear = 0
-        path.clear()
-        paths_local_destino.clear()
-        file_target.clear()
-        folderYear = ''
-        id_target = ''
-        check = 0
-
-
-    def setup_folders_files(dia=0,mes=0,ano=0, manual=False):
-
-        global folderYear
-        global id_target
-        global file_target
-        global paths_local_destino
-        global path_root
-
-        reset_folders()
-
-        if  manual:
-
-            folderYear = str(ano)
-            local_bases_folders(path_root, folderYear)
-            id_target = id_target_function((conversao_dia(dia, mes, ano)), delay=False)
-            file_target = names_file_target(id_target)
-            logs_bug("file_target in rotina manual", str(file_target))
-            paths_local_destino = paths_bases_globais(path_root, folderYear)
-
-        else:
-
-            folderYear=folder_year_function(day)
-            local_bases_folders(path_root,folderYear)
-            id_target=id_target_function(day)
-            file_target=names_file_target(id_target)
-            paths_local_destino=paths_bases_globais(path_root,folderYear)
-
-
-
-    def rotina_auto(loop=31,prin=True,only_check=False,day=0,delay=True): # rotina principal automatica
-
-        global paths_extracts
-        global folderYear
-        global id_target
-        global file_target
-        global paths_local_destino
-        global path_root
-
-        setup_folders_files()
-
-        if  only_check:
-
-            soma_files=0
-            i=0
-
-            for b1 in paths_local_destino:
-                list_dir=paths_local_destino[i]
-                try:
-                    list_dir_file=os.listdir(list_dir)
-                except FileNotFoundError:
-                    pass
-                try:
-                    list_dir_file.remove('extracts')
-                except ValueError:
-                    pass
-                number_files = len(list_dir_file)
-                soma_files=number_files+soma_files
-                logs_bug('path',str(paths_local_destino[i]))
-                logs_bug('number_files',str(number_files))
-                logs_bug('soma_files',str(soma_files))
-                i=i+1
-            return soma_files
-
-
-
-        else:
-            for a1 in range(loop): # variável bb determina quantos arquivos para trás podem ser baixados em modo automático
-
-                reset_folders()
-                folderYear = folder_year_function(day)
-                local_bases_folders(path_root, folderYear)
-
-                if delay:
-                    id_target=id_target_function(day)
-
-                else:
-                    id_target=id_target_function(day,delay=False)   #delay False utilizado para testes
-                file_target = names_file_target(id_target)
-                paths_local_destino = paths_bases_globais(path_root, folderYear)
-                i=-1
-
-                for exist in baseFolder:
-                    i=i+1
-                    logs_bug("paths é arquivo?",os.path.join(paths_local_destino[i],file_target[i]))
-                    logs_bug("is file",str(os.path.isfile(os.path.join(paths_local_destino[i],file_target[i]))))
-                    if not os.path.isfile(os.path.join(paths_local_destino[i],file_target[i])):
-                        logs_bug("fez download",str(os.path.isfile(os.path.join(paths_local_destino[i],file_target[i]))))
-                        if not only_check:
-                            try:
-                                if prin:
-                                    download_ftp("geoftp.ibge.gov.br",paths_local_destino,folderYear,id_target,file_target,i)
-                                    extracts(paths_extracts,paths_local_destino,file_target)
-                                else:
-                                    download_ftp("geoftp.ibge.gov.br",paths_local_destino,folderYear,id_target,file_target,i,prin=False)
-                                    extracts(paths_extracts,paths_local_destino,file_target,prin=False)
-                                    paths_extracts.clear()
-                            except gaierror:
-                                if prin:
-                                    print('Sem conexão com o servidor ftp://geoftp.ibge.gov.br')
-                                logs_info('Sem conexão com servidor ftp://geoftp.ibge.gov.br')
-                            except ftplib.error_perm:
-                                logs_info('Arquivo '+str(file_target[i])+' não encontrado')
-                                if prin:
-                                    print('Arquivo '+str(file_target[i])+' não encontrado')
-                            # try:)
-                            #     if prin:
-                            #         extracts(paths_extracts,paths_local_destino,file_target)
-                            #     else:
-                            #         extracts(paths_extracts,paths_local_destino,file_target,prin=False)
-
-                            except FileNotFoundError:
-                                pass
-                            except zipfile.BadZipFile:
-                                pass
-                    else:
-                        pass
-                    logs_info('Arquivo da base '+str(file_target[i])+' existente em: '+str(paths_local_destino[i]))
-
-                day=day+1
-
-    def rotina_base_especifica(dia,mes,ano): # rotina principal manual
-
-        global paths_extracts
-        global folderYear
-        global id_target
-        global file_target
-        global paths_local_destino
-        global path_root
-
-        setup_folders_files(dia,mes,ano,manual=True)
-        i=-1
-
-        for exist in baseFolder:
-            i=i+1
-            logs_bug('paths_local_destino[i]',str(paths_local_destino[i]))
-            if not os.path.isfile(os.path.join(paths_local_destino[i],file_target[i])):
-                try:
-                    download_ftp("geoftp.ibge.gov.br",paths_local_destino,folderYear,id_target,file_target,i)
-                except gaierror:
-                    print('Sem conexão com o servidor ftp://geoftp.ibge.gov.br')
-                    logs_info('Sem conexão com servidor ftp://geoftp.ibge.gov.br')
-                except ftplib.error_perm:
-                    logs_info('Arquivo '+file_target[i]+' não encontrado')
-                    print('Arquivo '+file_target[i]+' não encontrado')
-                try:
-                    extracts(paths_extracts,paths_local_destino,file_target)
-                    paths_extracts.clear()
-                except FileNotFoundError:
-                    pass
-                except zipfile.BadZipFile:
-                    logs_info('Arquivo '+file_target[i]+' não encontrado para extraçao')
-                    print('Erro de extração de dados, FileNotFoundError')
-            print('Arquivo da base '+file_target[i]+' existente em '+str(paths_local_destino[i]))
-
-        reset_folders()
-
-    # def thread2(name,th2):
-    #     schedule.every().day.at(th2).do(rotina_auto)
-    #     while True:
-    #         schedule.run_pending()
-    #         time.sleep(1*60)
-
-    def thread3(name,th3):
-        global check
-        check=rotina_auto(prin=False,only_check=True)
-
-
-
-    def interacao_user():
-
-        print('Entrando em modo manual')
-        time.sleep(1)
-        l2=True
-        while l2:
-            try:
-                dia=int(input('Qual dia da base para download? \n'))
-                if dia >31 or dia<=0:
-                    print('favor inserir valor entre 1 a 31')
-                    l2=True
-                    raise ValueError
-                else:
-                    l2=False
-            except ValueError:
-                print('Favor inserir apenas número correspondendo ao dia da base a ser baixada')
-                l2=True
-        l3=True
-        while l3:
-            try:
-                mes=int(input('Qual mês? (mês da data da coleta)\n'))
-                if mes >12 or mes<=0:
-                    print('favor inserir um valor que corresponde ao mês entre 1 a 12')
-                    raise ValueError
-                else:
-                    l3=False
-            except ValueError:
-                print('Favor inserir apenas número correspondendo o mês')
-                l3=True
-        l4=True
-        while l4:
-            try:
-                ano=int(input('Qual ano ?(ano da data da coleta)\n'))
-                conversao_dia(dia,mes,ano)
-                if (ano >=2016) and (ano<=now1.year):
-                    l4=False
-                    rotina_base_especifica(dia,mes,ano)
-                else:
-                    raise TypeError  # eu sei que não é TypeError, mas serviu...
-            except TypeError:
-                print('Favor inserir apenas número correspondente ao ano entre 2016 e ano atual.')
-                l4=True
-
-            except ValueError:
-                print('Data não existente, favor digitar uma data existente')
-                interacao_user()
-        lf=True
-        while lf:
-            print('Deseja fazer download de mais uma base?')
-            resp0=input('y/n\n')
-            try:
-                if resp0=='y' or resp0=='n':
-                    if resp0=='y':
-                        interacao_user()
-                    else:
-                        lf=False
-                        print('Deseja prosseguir para modo automático? ou finalizar?')
-                        print('digite y para prossegui e n para finalizar?')
-                        resp1 = input()
-                        try:
-                            if resp1=='y' or resp0=='n':
-                                if resp1 == 'y':
-                                    segunda_etapa()
-                                if resp1=='n':
-                                    print('Fim da aplicação')
-                                    os._exit(1)
-                                else:
-                                    ValueError
-                        except ValueError:
-                            print('Favor responda somente y para sim ou n para não')
-                else:
-                    raise ValueError
-            except ValueError:
-                print('Favor responda somente y para sim ou n para não')
-
-    def show_files():
-
-        global path_root
-
-        print('\nExibindo arquivos contidos em %s:\n' %path_root)
-        time.sleep(2)
-        setup_folders_files()
-        i=-1
-        for ai in paths_local_destino:
-            i=i+1
-            path.append(paths_local_destino[i])
-            try:
-                print('\nArquivos contidos em: '+str(path[i])+str(os.listdir(path[i])))
-                logs_info('Arquivos contidos em: '+str(path[i])+str(os.listdir(path[i])))
-            except FileNotFoundError:
-                pass
-
-# def interrupted(signum, frame): # funciona apenas no linux
-#      "called when read times out"
-#     print ('Aplicação finalizada')
-#     sys.exit()
-
-# def watchdog():
-#     segunda_etapa()
-# # time.sleep(10)
-# #   os._exit(1)
-
-# ----------------------------------------------------fluxo principal ---------------------------------------------------------#
-#
-#
-# # Primeira etapa --------------------------------------------------------------------------------------------------------------#
-#
-# def primeira_etapa():
-#
-#     global delay_time
-#
-#     delay_time = 30   # delay time in seconds
-#     alarm = threading.Timer(delay_time, watchdog)
-#     alarm.start()
-#     print('\nDigite enter  para iniciar\n')
-#     print('Se %d segundos não houver intereção, a aplicação irá para modo automático'% delay_time)
-#     # print('Digite enter para continuar e escolher uma base para download')
-#     input()
-#     # disable the alarm after success
-#
-#     l0=True
-#     while l0:
-#         print('\n\nDigite 1 para escolher uma base específica')
-#         print('Digite 2 para iniciar modo automático')
-#         r0=int(input())
-#         try:
-#             if r0==1 or r0==2:
-#                 if r0==1:
-#                     l0=False
-#                     alarm.cancel()
-#                     interacao_user()
-#                 else:
-#                     l0=False
-#                     alarm.cancel()
-#                     segunda_etapa()
-#                     break
-#             else:
-#                 l0=True
-#                 raise ValueError
-#         except ValueError:
-#             print('Por favor, responda somente 1 ou 2 para os opções')
-#
-#
-#
-#
-#
-#
-# # Segunda etapa --------------------------------------------------------------------------------------------------------------#
-# def segunda_etapa():
-#
-#
-#     global check
-#     global loop
-#     global path_root
-#
-#     # verifica quantos arquivos de base há no em local
-#     t3 = threading.Thread(target=thread3, args=('task13', 'none'))
-#     t3.start()
-#     print('As bases por este programa, serão baixadas e descompactadas automaticamente em %s\n' %path_root)
-#     time.sleep(2)
-#     print('\nEm %s' %path_root)
-#     time.sleep(4)
-#     print('\nAguarde enquanto faremos algumas verificações')
-#     time.sleep(3)
-#     t3.join()
-#     if check < 0:
-#         check = 0
-#     print('\nFoi verificado que há ao todo há %d bases em C:\IBGE\n ' % check)
-#     time.sleep(2)
-#     print('Verificando se há arquivos recentes no servidor do IBGE para serem baixados\n')
-#     time.sleep(2)
-#     rotina_auto(loop, prin=True, only_check=False)
-#     print('Foi verificado e atualizado os arquivos recentes com sucesso')
-#     show_files()
-#     time.sleep(6)
-#     os._exit(1)
-#
-#
-# # primeira_etapa()
--- source/main1.py
+++ source/main1.py
@@ -17,6 +17,7 @@ import autoRmbclib1
 from tkinter import*
 from tkinter import ttk
 from tkinter import filedialog
+import tkinter.messagebox
 import os
 import time
 import datetime
@@ -24,21 +25,41 @@ import threading
 import tkcalendar
 import locale
 
-root=Tk()
+
+
 #configurações iniciais
+root=Tk()
+
+
 y_text_canvas=10
 x_text_canvas=70
 bases_escolhidas=[]
 tags_bases=0
 tags_vetor=[]
 version="versão 2.0"
-i1=autoRmbclib1.RbmcLib()
-now1 = i1.now1
-year=str(now1.year)
-month=str(now1.month)
-day=str(now1.day)
-local_folders=StringVar()
-local_folders.set("")
+
+os1=os.name
+if os.name=="nt":
+    root.iconbitmap("alvo.ico")
+else:
+    # root.iconbitmap("@alvo2.xbm") // desisto, não esta funcionando no linux
+    pass
+
+
+def reset():
+
+    global globaly_text_canvas
+    global x_text_canvas
+    global bases_escolhidas
+    global tags_bases
+    global tags_vetor
+    global version
+    y_text_canvas=10
+    x_text_canvas=10
+    tags_bases=0
+    tags_vetor=[]
+    version="versão 2.0"
+    bases_escolhidas.clear()
 
 
 def update():
@@ -74,9 +95,12 @@ def add_button_text():
     global bases_escolhidas
     global tags_bases
     global tags_vetor
+    x_text_canvas=10
+    dataText=['']
 
     bb1=True
     aa1=c1.get()
+
     i=0
     for j in bases_escolhidas:
         if aa1==bases_escolhidas[i]:
@@ -84,10 +108,11 @@ def add_button_text():
         i=i+1
     if bb1:
         bases_escolhidas.append(c1.get())
-        tags_vetor.append(canvas.create_text(x_text_canvas,y_text_canvas,font="Times 12 italic bold",text=bases_escolhidas[-1],tag=str(tags_bases)))
+        dataText.append(str(cal.selection_get().day)+"/"+str(cal.selection_get().month)+"/"+str(cal.selection_get().year))
+        tags_vetor.append(canvas.create_text(x_text_canvas,y_text_canvas,anchor='w',justify='center', font="Times 12 italic bold",text=bases_escolhidas[-1]+"          "+str(dataText[-1]),tag=str(tags_bases)))
         tags_bases=tags_vetor[-1]
         y_text_canvas=y_text_canvas+20
-    print(bases_escolhidas)
+
 
 
 def revomer_button_todos():
@@ -100,7 +125,7 @@ def revomer_button_todos():
     y_text_canvas=10
     x_text_canvas=70
     global bases_escolhidas
-    print(str(tags_bases))
+
     bases_escolhidas.clear()
     canvas.delete(ALL)
     canvas.update()
@@ -119,21 +144,53 @@ def revomer_button():
         pass
     canvas.delete(tags_vetor[tags_bases-1])
     tags_bases=tags_bases-1
-    print(str(tags_vetor))
 
 
 def download_button():
+    t3 = threading.Thread(target=threading_download, args=())
+    t3.start()
+
+def threading_download():
+
 
     global bases_escolhidas
     global local_folders
     bases_escolhidas_download=[]
     cal1=calButtonMouse()
     cal2_converted=i1.convert_to_doy(cal1)
+
     # verificação se foi seleciando pelo menos uma data
     string_data_corrente="%s%s%s" %(day,month,year)
     string_cal1=str(cal1.day)+str(cal1.month)+str(cal1.year)
+
+    # mensagens de falta de parâmetros
+
     if string_cal1==string_data_corrente:
-        print("Aqui vai uma mensagem de alerta, favor selecionar a data de levantamento")
+        tkinter.messagebox.showerror("Falta de parâmetros",'FAVOR INFORMAR A DATA DO LEVANTAMENTO (não pode ser a data de hoje)')
+        return False
+        # try:
+        #     root.destroy()
+        # except UnboundLocalError:
+        #     pass
+        # root=Tk()
+
+    if local_folders.get()=='':
+        tkinter.messagebox.showerror("Falta de parâmetros",'FAVOR INFORMAR O LOCAL ONDE O ARQUIVO SERÁ SALVO EM SEU COMPUTADOR')
+        return False
+        # try:
+        #     root.destroy()
+        # except UnboundLocalError:
+        #     pass
+        # root=Tk()
+
+    if len(bases_escolhidas)==0:
+        tkinter.messagebox.showerror("Falta de parâmetros",'FAVOR INFORMAR UMA BASE PARA DOWNLOAD')
+        return False
+        # try:
+        #     root.destroy()
+        # except UnboundLocalError:
+        #     pass
+        # root=Tk()
 
     if cal2_converted>=10 and cal2_converted<100:
         cal2_converted="0"+str(cal2_converted)
@@ -144,15 +201,25 @@ def download_button():
 
     bases_escolhidas_download=i1.names_file_target(cal2_converted,bases_escolhidas)
     id_target=cal2_converted
-    print(bases_escolhidas_download)
-    print(id_target)
+
     i=0
+
+
     for j in bases_escolhidas:
         i1.download_ftp(local_folders.get(),cal1.year,id_target,bases_escolhidas_download,i)
-        print(bases_escolhidas[i])
+        tkinter.messagebox.showinfo('Sucesso','Concluído download ' +str(bases_escolhidas_download[i]+' com sucesso!'))
         i=i+1
 
 
+
+reset()
+i1=autoRmbclib1.RbmcLib()
+now1 = i1.now1
+year=str(now1.year)
+month=str(now1.month)
+day=str(now1.day)
+local_folders=StringVar()
+local_folders.set("")
 #Threading de update
 clock = threading.Timer(30, update)
 clock.start()
@@ -161,14 +228,6 @@ clock.start()
 root.resizable(width=False, height=False)
 root.title("AutoRbmcIbge2")
 
-os1=os.name;
-if os.name=="nt":
-    root.iconbitmap("alvo.ico")
-else:
-    # root.iconbitmap("@alvo2.xbm") // desisto, não esta funcionando no linux
-    pass
-
-# frames
 frame1=Frame() #superior
 frame2=Frame() # inferior
 frame3=Frame()# canvas
@@ -185,53 +244,53 @@ separator2.grid(row=1,column=2,sticky="we")
 #frames
 frame1.grid(row=0,column=0)# ComboBox e botoes
 frame2.grid(row=2,column=0,sticky="nw")# Calendarios e Labels
-frame3.grid(row=2,column=1,sticky="s")# Canvas
+frame3.grid(row=2,column=1,sticky="w")# Canvas
 frame5.grid(row=0,column=1)# labels de hoje
 frame6.grid(row=2,column=2,sticky="n")# botões
+
 #Canvas
 canvas = Canvas(frame3, bg='white', width=260, height=300)
-canvas.grid(row=1,column=0)
+canvas.grid(row=2,column=0)
 
 
-# Botões
+    # Botões
 
 b1=Button(frame1,text="Adicionar base",command=add_button_text)
 b2=Button(frame1,text="Salvar em:",command=browse_button)
-# b3=Button(frame1,text="Extrair em:")
+    # b3=Button(frame1,text="Extrair em:")
 
 b4=Button(frame6,text="Remover",command=revomer_button)
 b5=Button(frame6,text="Remover todos",command=revomer_button_todos)
 b6=Button(frame6,text="Download",font=("Helvetica", 10),fg="red",command=download_button)
 
-#frame1
+    #frame1
 b1.grid(row=2,column=4,pady=5)#Adicionar
 b2.grid(row=4,column=4,pady=5)#Salvar em:,
-# b3.grid(row=6,column=4,pady=5)#Extrair em:
+
 
 #frame6
-b4.grid(row=0,column=0,sticky="wn")#Remover
+b4.grid(row=1,column=0,pady=30)#Remover
 b5.grid(row=3,column=0,sticky="n",pady=20)#Remover todos
 l55=Label(frame6)
 l56=Label(frame6)
 l57=Label(frame6)
-l55.grid(row=2,column=0)#branco
-#l56.grid(row=3,column=0)#branco
+
 l57.grid(row=4,column=0,pady=20)#branco
 l57.grid(row=4,column=0,pady=50)#branco
-b6.grid(row=5,column=0,pady=10)
+b6.grid(row=5,column=1,pady=10)
 
 b6.grid(row=5,column=0,sticky="S",pady=10)
-# ComboBox
+    # ComboBox
 c1=ttk.Combobox(frame1,width=30)
 c1.grid(row=2,column=2,sticky="w")
 c1['values']=i1.sigla
 c1.current(2)
-# Calendar
+    # Calendar
 cal=tkcalendar.Calendar(frame2)
 cal.bind('<<CalendarSelected>>',calButtonMouse)
 cal.grid(row=2,column=0,sticky="w")
 
-# Labels
+    # Labels
 l1=Label(frame1,text="AutoRbmcIbge",font=("Helvetica", 20))#fixo
 l2=Label(frame1,text="Bases")#fixo
 l3=Label(frame1,text=version)#fixo
@@ -246,7 +305,8 @@ l10=Label(frame2,text="Data do levantamento:")
 l11=Label(frame2,text='0')# data do levantamento
 l12=Label(frame2,text="Dia do ano do levantamento:")
 l13=Label(frame2,text='0')
-l14=Label(frame3,text='Base para download')
+l14=Label(frame3,text='Base e data para download')
+# l15=Label(frame3,text='Data')
 
 l1.grid(row=0,column=5)
 l2.grid(row=1,column=2,sticky="w")
@@ -255,16 +315,13 @@ l4.grid(row=2,column=5,sticky="w") #Data atual
 l6.grid(row=2,column=6)#Data
 l5.grid(row=3,column=5)# dia de ano hoje
 l7.grid(row=3,column=6,sticky="e")# dia de ano
-# l8.grid(row=4,column=9) # testando update
+    # l8.grid(row=4,column=9) # testando update
 l9.grid(row=4,column=5,sticky="e")
-#frame 2
+    #frame 2
 l10.grid(row=0,column=0,sticky="w")# data do levantamento
 l11.grid(row=0,column=1,sticky="w")# data do levantamento
 l12.grid(row=1,column=0,sticky="w")# dia do ano do levantamento
 l13.grid(row=1,column=1,sticky="w")#dia do ano do levantamento
-l14.grid(row=0,column=0,)
-
-
-
-
+l14.grid(row=0,column=0,sticky="w")
+# l15.grid(row=0,column=0,sticky="e")
 root.mainloop()
